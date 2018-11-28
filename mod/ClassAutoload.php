55c04b91ccf8242510e92abdbdf755336881d684
Y.Petremann <y.petremann@gmail.com>
2018-11-28 15:57:28 +0100

    Cleanup ClassAutoload
    Removed debug instructions

--- mod/ClassAutoload.php
+++ mod/ClassAutoload.php
@@ -3,7 +3,6 @@ class ClassAutoload {
     public static function register() {
         spl_autoload_register(function($class) {
             $file = str_replace('\\', DIRECTORY_SEPARATOR, 'mod\\'.$class).'.php';
-            echo $file;
             if (file_exists($file) and is_file($file)) {
                 require $file;
                 return true;
