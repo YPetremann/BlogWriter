b7fdf1a2422c0a6b10b58cc21b0c757e1851f7e5
Y.Petremann <y.petremann@gmail.com>
2018-11-29 16:00:58 +0100

    Add First View
    Added first implementations of views used for thuis project, use stub data.

--- index.php
+++ index.php
@@ -1,15 +1,62 @@
 <?php
 require('mod/ClassAutoload.php');
 ClassAutoload::register();
+function url($url){
+    return "/BlogWriter".$url;
+}
+
+$header  = include "dat/view/header.phtml";
+$footer  = include "dat/view/footer.phtml";
 $router = new Router($_GET["url"]);
-$router->all('/admin', function(){
-    echo "Page de connexion";
+
+$router->all('/login', function(){
+    global $header, $footer;
+    $content = include "dat/view/UserLogin.phtml";
+    include "dat/view/main.phtml";
 });
+
 $router->all('/posts/:id', function($id){
-    echo "Voila l'article $id";
-});
+    global $header, $footer;
+    /* TODO only there to test view */ //*
+    $post = [
+        "title"=>"Chapitre 1",
+        "post_date_fr"=>"20/11/2018",
+        "content"=>"Contenu du blog",
+        "id"=>1,
+    ];
+    $comments = [
+        [
+            "id"=>1,
+            "author" => "Anon",
+            "comment_date_fr"=>"20/11/2018",
+            "content"=>"Voici du contenu",
+        ]
+    ];
+    // */
+
+    $content = include "dat/view/BlogPost.phtml";
+    include "dat/view/main.phtml";});
 $default = function(){
-    echo "Voila la liste d'articles";
+    global $header, $footer;
+    /* TODO only there to test view */ //*
+    $posts = [
+        [
+            "id"=>1,
+            "title"=>"Chapitre 1",
+            "post_date_fr"=>"20/11/2018",
+            "content"=>"Contenu du blog",
+        ],
+        [
+            "id"=>2,
+            "title"=>"Chapitre 2",
+            "post_date_fr"=>"20/11/2018",
+            "content"=>"Contenu du blog",
+        ]
+    ];
+    // */
+
+    $content = include "dat/view/BlogList.phtml";
+    include "dat/view/main.phtml";
 };
 $router->all('/posts', $default);
 $router->default($default);
