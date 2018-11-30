896310274d39b457c7df34b2b5e4c02c9acac203
Y.Petremann <y.petremann@gmail.com>
2018-11-30 12:03:22 +0100

    Delegating blog actions to Blog\Controller

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
 require('mod/ClassAutoload.php');
 ClassAutoload::register();
-function url($url, $unique=false){
+function url($url, $unique=false)
+{
     $prefix = "/BlogWriter";
     $url = $prefix . $url;
-    (!$unique)?: $url .= "?v=".uniqid('',true);
+    (!$unique)?: $url .= "?v=".uniqid('', true);
     return $url;
 }
 
@@ -12,78 +13,31 @@ $header  = include "dat/view/header.phtml";
 $footer  = include "dat/view/footer.phtml";
 $router = new Router($_GET["url"]);
 
-$router->all('/login', function(){
+$router->all('/login', function () {
     global $header, $footer;
     $content = include "dat/view/UserLogin.phtml";
     include "dat/view/main.phtml";
 });
 
-$router->all('/posts/:id', function($id){
+$router->all('/posts/:id', function ($id) {
     global $header, $footer;
-    /* TODO only there to test view */ //*
-    $post = [
-        "title"=>"Chapitre 1",
-        "post_date_fr"=>"20/11/2018",
-        "content"=>"Contenu du blog",
-        "id"=>1,
-    ];
-    $comments = [
-        [
-            "id"=>1,
-            "author" => "Anon",
-            "comment_date_fr"=>"20/11/2018",
-            "content"=>"Voici du contenu",
-        ],
-        [
-            "id"=>1,
-            "author" => "Anon",
-            "comment_date_fr"=>"20/11/2018",
-            "content"=>"Voici du contenu",
-        ],
-        [
-            "id"=>1,
-            "author" => "Anon",
-            "comment_date_fr"=>"20/11/2018",
-            "content"=>"Voici du contenu",
-        ],
-        [
-            "id"=>1,
-            "author" => "Anon",
-            "comment_date_fr"=>"20/11/2018",
-            "content"=>"Voici du contenu",
-        ],
-        [
-            "id"=>1,
-            "author" => "Anon",
-            "comment_date_fr"=>"20/11/2018",
-            "content"=>"Voici du contenu",
-        ]
-    ];
-    // */
+
+    $blog = new Blog\Controller();
+    $post = $blog->post($id);
 
     $content = include "dat/view/BlogPost.phtml";
-    include "dat/view/main.phtml";});
-$default = function(){
+    include "dat/view/main.phtml";
+});
+
+$default = function () {
     global $header, $footer;
-    /* TODO only there to test view */ //*
-    $posts = [
-        [
-            "id"=>1,
-            "title"=>"Chapitre 1",
-            "post_date_fr"=>"20/11/2018",
-            "content"=>"Contenu du blog",
-        ],
-        [
-            "id"=>2,
-            "title"=>"Chapitre 2",
-            "post_date_fr"=>"20/11/2018",
-            "content"=>"Contenu du blog",
-        ]
-    ];
-    // */
+
+    $blog = new Blog\Controller();
+    $posts = $blog->lists();
 
     $content = include "dat/view/BlogList.phtml";
     include "dat/view/main.phtml";
 };
+
 $router->all('/posts', $default);
 $router->default($default);
