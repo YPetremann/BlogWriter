bf52acbbf493a52e001fe5f9fa7f22bc4528d8b7
Y.Petremann <y.petremann@gmail.com>
2018-12-04 19:13:22 +0100

    Added View Permit to store content ready for display to help build the view.

--- index.php
+++ index.php
@@ -1,54 +1,77 @@
 <?php
 require('mod/ClassAutoload.php');
 ClassAutoload::register();
+
 function url($url, $unique=false)
 {
     $prefix = "/BlogWriter";
     $url = $prefix . $url;
-    (!$unique)?: $url .= "?v=".uniqid('', true);
+    (!$unique) ?: $url .= "?v=".uniqid('', true);
     return $url;
 }
-$head = "";
-$header  = include "dat/view/header.phtml";
-$footer  = include "dat/view/footer.phtml";
+
 $router = new Router($_GET["url"]);
+$view = new View();
+$view->header = include "dat/view/header.phtml";
+$view->footer = include "dat/view/footer.phtml";
 
 // Login page, Display login form
 $router->all('/login', function () {
-    global $header, $footer, $content, $title;
+    global $view;
+    $blog = new Blog\Controller();
+    $view->content = include "dat/view/UserLogin.phtml";
+});
 
-    $content = include "dat/view/UserLogin.phtml";
+// Edit post page, Permit to edit post
+$router->post('/posts/:id/edit', function ($id) {
+    global $view;
+    if($_SESSION["user"]->post_can_create)
+    $blog = new Blog\Controller();
+    $post = $blog->readPost($id);
+
+    $view->content = include "dat/view/BlogPostEdit.phtml";
+});
+
+$router->all('/posts/:id/edit', function ($id) {
+    global $view;
+    if($_SESSION["user"]->post_can_create)
+    $blog = new Blog\Controller();
+    $post = $blog->readPost($id);
+
+    $view->content = include "dat/view/BlogPostEdit.phtml";
 });
 
 // Post page, Display post and comments
 $router->post('/posts/:id', function ($id) {
-    global $header, $footer, $content, $title;
+    global $view;
 
     $blog = new Blog\Controller();
     $post = $blog->createComment($id, $_POST);
     $post = $blog->readPost($id);
 
-    $content = include "dat/view/BlogPost.phtml";
+    $view->content = include "dat/view/BlogPost.phtml";
 });
 $router->all('/posts/:id', function ($id) {
-    global $header, $footer, $content, $title;
+    # TODO gerer cas ou router
+    global $view;
 
     $blog = new Blog\Controller();
     $post = $blog->readPost($id);
 
-    $content = include "dat/view/BlogPost.phtml";
+    $view->content = include "dat/view/BlogPost.phtml";
 });
 
 // Home page, display blog post list
 $default = function () {
-    global $header, $footer, $content, $title;
+    global $view;
 
     $blog = new Blog\Controller();
-    $posts = $blog->lists();
+    $posts = $blog->listPost();
 
-    $content = include "dat/view/BlogList.phtml";
+    $view->content = include "dat/view/BlogList.phtml";
 };
 
 $router->all('/posts', $default);
 $router->default($default);
-include "dat/view/main.phtml";
+
+echo include "dat/view/main.phtml";
