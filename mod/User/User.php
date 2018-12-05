57725a038941bdf73a802b473a5c338706d5bcff
Y.Petremann <y.petremann@gmail.com>
2018-12-05 17:54:00 +0100

    Update User

--- mod/User/User.php
+++ mod/User/User.php
@@ -3,6 +3,9 @@ namespace User;
 
 class User implements \Blog\UserBlogI
 {
+    protected $type;
+    protected $id = 0;
+    protected $name = "Utilisateur Anonyme";
     use \Blog\UserBlogT;
     public function __get($name) {
         $method = 'get_'.$name;
@@ -12,4 +15,8 @@ class User implements \Blog\UserBlogI
             return "--";
         }
     }
+
+    public function get_id() {return $this->id;}
+    public function get_name() {return $this->name;}
+    public function get_type() {return $this->type;}
 }
