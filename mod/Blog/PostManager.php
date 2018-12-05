55354de36015fc9316e9cae32a9d5275efe0051f
Y.Petremann <y.petremann@gmail.com>
2018-12-05 17:56:35 +0100

    Display list depending of user rights in PostManager

--- mod/Blog/PostManager.php
+++ mod/Blog/PostManager.php
@@ -40,6 +40,12 @@ class PostManager extends DBManager
     }
     public function list()
     {
+        $cond = [];
+        if ( $this->user->post_can_read & (UserBlogI::PUBLIC)){ array_push($cond, "p.visibility = 1"); }
+        if ( $this->user->post_can_read & (UserBlogI::PRIVATE)){ array_push($cond, "p.visibility = 0"); }
+        if ( $this->user->post_can_read & (UserBlogI::SELF)){ array_push($cond, "p.author_id = ".$this->user->id); }
+        if ( $this->user->post_can_read & (UserBlogI::OTHER)){ array_push($cond, "p.author_id != ".$this->user->id); }
+        if ( !empty($cond) ){ $cond = 'WHERE '.join(' OR ',$cond); }
         $query = $this->db->query('SELECT
                                p.id id,
                                p.title title,
@@ -49,6 +55,7 @@ class PostManager extends DBManager
                              FROM posts p
                              INNER JOIN users u
                              ON p.author_id = u.id
+                             '.$cond.'
                              ORDER BY p.id DESC');
         $data = $query->fetchAll();
         $query->closeCursor();
