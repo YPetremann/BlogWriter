314358cdceba04d0a207629ebb4d950628a61734
Y.Petremann <y.petremann@gmail.com>
2018-12-05 21:34:06 +0100

    Refactor Blog Controller - add exceptions

--- mod/Blog/Controller.php
+++ mod/Blog/Controller.php
@@ -1,18 +1,43 @@
 <?php
 namespace Blog;
 class Controller {
+    public function __construct($as){
+        $this->user = $as;
+    }
     public function listPost(){
-        $postManager = new PostManager($_SESSION["user"]);
-        return $postManager->list();
+        global $view;
+        try {
+            $postManager = new PostManager($this->user);
+            $posts = $postManager->list();
+            $view->content = include "dat/view/BlogList.phtml";
+        } catch(\Exception $e) {
+            $view->message .= '<div class="error"><div class="fixer">'.$e->getMessage().'</div></div>';
+            return false;
+        }
     }
     public function createComment($id, $post) {
-        $id = (int) $id;
-        $author = htmlspecialchars(Strip_tags((string) $post['author'] ?: ""));
-        $comment = nl2br(htmlspecialchars((string) $post['comment'] ?: ""));
+        global $view;
+        try {
+            $id = (int) $id;
+            $comment = nl2br(htmlspecialchars((string) $post['comment'] ?: ""));
 
-        if (empty($author) or empty($comment)) {
-            throw new Exception('Tous les champs ne sont pas remplis !');
+            if (empty($comment)) {
+                throw new \Exception('Tous les champs ne sont pas remplis !');
+            }
+
+            $commentManager = new CommentManager($this->user);
+            $affectedLines = $commentManager->create($id, $this->user->id, $comment);
+
+            if (!$affectedLines) {
+                return new \Exception("Impossible d'ajouter le commentaire !");
+            } else {
+                header('Location: '.$_SERVER['REQUEST_URI']);
+            }
+        } catch(\Exception $e) {
+            $view->message .= '<div class="error"><div class="fixer">'.$e->getMessage().'</div></div>';
+            return false;
         }
+    }
 
         $commentManager = new CommentManager($_SESSION["user"]);
         $affectedLines = $commentManager->create($id, $author, $comment);
@@ -25,14 +50,19 @@ class Controller {
         return true;
     }
     public function readPost($id) {
-        $id = (int) $id;
-        $postManager = new PostManager($_SESSION["user"]);
+        global $view;
+        try {
+            $id = (int) $id;
+            $postManager = new PostManager($this->user);
+            $post = $postManager->read($id);
 
-        $post = $postManager->read($id);
+            $commentManager = new CommentManager($this->user);
+            $post['comments'] = $commentManager->list($id);
 
-        $commentManager = new CommentManager($_SESSION["user"]);
-        $post['comments'] = $commentManager->list($id);
-
-        return $post;
+            $view->content = include "dat/view/BlogPost.phtml";
+        } catch(\Exception $e) {
+            $view->message .= '<div class="error"><div class="fixer">'.$e->getMessage().'</div></div>';
+            return false;
+        }
     }
 }
